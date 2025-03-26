26ee38fb524bf681aae1972f5eb22701cb5bdb07
Ciara Donegan <82416895+ciara-donegan@users.noreply.github.com>
2025-03-26 10:04:17 -0400

    SO2 and SO4 plot updates
    Updates to loadso2/so4 plots, deposition plots

--- code/additional_paper_plots.R
+++ code/additional_paper_plots.R
@@ -44,6 +44,12 @@ axis_title_font <- 12
 #filters each species from summary_long
 loadso4 <- filter_species(summary_long, "loadso4")
 loadso2 <- filter_species(summary_long, "loadso2")
+dryso2_diff <- filter_species(summary_long, "dryso2")
+wetso2_diff <- filter_species(summary_long, "wetso2")
+dryso4_diff <- filter_species(summary_long, "dryso4")
+wetso4_diff <- filter_species(summary_long, "wetso4")
+rsut <- filter_species(summary_long,"rsut")
+rlut <- filter_species(summary_long,"rlut")
 
 if (sort_by=="region") {
   #creates plots based on each species using the plot_species function
@@ -63,6 +69,111 @@ loadso4_diff <- loadso4_plot
 
 loadso2_diff_df <- loadso2
 loadso4_diff_df <- loadso4
+
+## Generate loadso4/net rad linear regressions
+net_rad <- dplyr::left_join(rlut, rsut, by = c("model", "region"))
+net_rad <- dplyr::mutate(net_rad, value = value.x + value.y) %>%
+  dplyr::mutate(sd = sqrt(sd.x^2 + sd.y^2)) %>%
+  dplyr::select(c(model, region, value, sd))
+
+loadso4_netrad_combined <- dplyr::left_join(loadso4,net_rad, by = c("model","region"))
+loadso4_netrad_combined <- drop_na(loadso4_netrad_combined)
+loadso4_netrad_combined <- loadso4_netrad_combined %>% filter(model!="GEOS")
+loadso4_netrad_plot <- ggplot(loadso4_netrad_combined, aes(value.x,value.y,color=model)) +
+  geom_point() +
+  
+  # trend lines
+  geom_smooth(method=lm,data=filter(loadso4_rsut_combined,model=="CESM1"),se=FALSE, linetype = "dashed") +
+  geom_smooth(method=lm,data=filter(loadso4_rsut_combined,model=="CAM-ATRAS"),se=FALSE, linetype = "dashed") +
+  geom_smooth(method=lm,data=filter(loadso4_rsut_combined,model=="E3SM"),se=FALSE, linetype = "dashed") +
+  #geom_smooth(method=lm,data=filter(loadso4_rsut_combined,model=="GEOS"),se=FALSE, linetype = "dashed") +
+  geom_smooth(method=lm,data=filter(loadso4_rsut_combined,model=="GFDL-ESM4"),se=FALSE, linetype = "dashed") +
+  geom_smooth(method=lm,data=filter(loadso4_rsut_combined,model=="GISS-E2.1"),se=FALSE, linetype = "dashed") +
+  geom_smooth(method=lm,data=filter(loadso4_rsut_combined,model=="NorESM2"),se=FALSE, linetype = "dashed") +
+  
+  # aesthetics
+  scale_color_manual(values = model_colors) +
+  #facet_wrap(vars(model)) +
+  geom_errorbar(aes(ymin=value.y-sd.y,ymax=value.y+sd.y),width=0) +
+  geom_errorbarh(aes(xmin=value.x-sd.x,xmax=value.x+sd.x),height=0) +
+  #xlim(-10000,10000) +
+  #ylim(-1,0.6) +
+  ylab(expression(Delta~net~radiative~flux~(W~m^-2))) +
+  xlab(expression(Delta~SO4~column~burden~(kg~m^-2)))
+
+# Get values in table
+data <- loadso4_netrad_combined
+data_name <- "loadso4_netrad_combined"
+
+# Split data by model
+data_CAMATRAS <- data[data$model=="CAM-ATRAS",]
+data_CESM1 <- data[data$model=="CESM1",]
+data_E3SM <- data[data$model=="E3SM",]
+data_GFDL <- data[data$model=="GFDL-ESM4",]
+data_GISS <- data[data$model=="GISS-E2.1",]
+data_NorESM <- data[data$model=="NorESM2",]
+
+# Linear regressions
+model_CAMATRAS <- lm(value.y ~ value.x, data = data_CAMATRAS)
+r_CAMATRAS <- summary(model_CAMATRAS)$r.squared
+intercept_CAMATRAS <- model_CAMATRAS$coefficients[1]
+slope_CAMATRAS <- model_CAMATRAS$coefficients[2]
+
+model_CESM1 <- lm(value.y ~ value.x, data = data_CESM1)
+r_CESM1 <- summary(model_CESM1)$r.squared
+intercept_CESM1 <- model_CESM1$coefficients[1]
+slope_CESM1 <- model_CESM1$coefficients[2]
+
+model_E3SM <- lm(value.y ~ value.x, data = data_E3SM)
+r_E3SM <- summary(model_E3SM)$r.squared
+intercept_E3SM <- model_E3SM$coefficients[1]
+slope_E3SM <- model_E3SM$coefficients[2]
+
+model_GFDL <- lm(value.y ~ value.x, data = data_GFDL)
+r_GFDL <- summary(model_GFDL)$r.squared
+intercept_GFDL <- model_GFDL$coefficients[1]
+slope_GFDL <- model_GFDL$coefficients[2]
+
+model_GISS <- lm(value.y ~ value.x, data = data_GISS)
+r_GISS <- summary(model_GISS)$r.squared
+intercept_GISS <- model_GISS$coefficients[1]
+slope_GISS <- model_GISS$coefficients[2]
+
+model_NorESM <- lm(value.y ~ value.x, data = data_NorESM)
+r_NorESM <- summary(model_NorESM)$r.squared
+intercept_NorESM <- model_NorESM$coefficients[1]
+slope_NorESM <- model_NorESM$coefficients[2]
+
+# Put values in table
+models <- c("CAM-ATRAS","CESM1","E3SM","GFDL-ESM4","GISS-E2.1","NorESM2")
+slopes <- c(slope_CAMATRAS,slope_CESM1,slope_E3SM,slope_GFDL,slope_GISS,slope_NorESM) %>% as.numeric()
+intercepts <- c(intercept_CAMATRAS,intercept_CESM1,intercept_E3SM,intercept_GFDL,intercept_GISS,intercept_NorESM) %>% as.numeric()
+r_squared <- c(r_CAMATRAS,r_CESM1,r_E3SM,r_GFDL,r_GISS,r_NorESM)
+
+table <- data.frame(models=models,
+                    slopes=slopes,
+                    intercepts=intercepts,
+                    r_squared=r_squared)
+
+# Note: R2 is much less reliable across regions with one experiment, than across experiments with one region
+# Makes sense that different regions are impacted differently
+
+if (sort_by=="experiment") {
+  #creates plots based on each species using the plot_species function
+  net_rad_filtered <- net_rad %>% filter(model!="GEOS")
+  rsut_filtered <- rsut %>% filter(model!="GEOS")
+  rlut_filtered <- rlut %>% filter(model!="GEOS")
+  
+  rlut_plot  <- plot_species(rlut_filtered, region, value, 'Upwelling Longwave Flux', expression(Delta*~rlut~(W~m^-2)), exper, model_colors, model_symbols)
+  rsut_plot  <- plot_species(rsut_filtered, region, value, 'Upwelling Shortwave Flux', expression(Delta*~rsut~(W~m^-2)), exper, model_colors, model_symbols)
+  net_rad_plot  <- plot_species(net_rad_filtered, region, value, 'Net Radiative Flux', expression(Delta*~net_rad~(W~m^-2)), exper, model_colors, model_symbols)
+}
+
+# Save so2/so4 plots together
+flux_plots <- grid_arrange_shared_legend(rlut_plot,rsut_plot,net_rad_plot,ncol=3)
+png(paste0(emi_dir,"/output/flux_plots_shp-60p-red.png"))
+grid.draw(flux_plots)
+dev.off()
 
 ## Regenerate plots using per-diff
 diff_or_perdiff <- 'per-diff'
@@ -174,3 +285,94 @@ all_sox_plots_grid <- plot_grid(all_sox_plots,legend,ncol=3,rel_heights=c(4,5))
 png(paste0(emi_dir,"/output/all_SOx_plots_shp-60p-red.png"))
 grid.draw(all_sox_plots_grid)
 dev.off()
+
+## Total deposition over basins
+dryso2 <- dryso2_diff
+wetso2 <- wetso2_diff
+dryso4 <- dryso4_diff
+wetso4 <- wetso4_diff
+
+# Multiply by area of basin and 1e-9 (kg -> Tg), to get units in Tg over basin
+dryso2$value[dryso2$region=="global"] <- dryso2$value[dryso2$region=="global"]*global_area*31536000
+dryso2$value[dryso2$region=="sea"] <- dryso2$value[dryso2$region=="sea"]*sea_area*31536000
+dryso2$value[dryso2$region=="land"] <- dryso2$value[dryso2$region=="land"]*land_area*31536000
+dryso2$value[dryso2$region=="NH_atlantic"] <- dryso2$value[dryso2$region=="NH_atlantic"]*atlantic_area*31536000
+dryso2$value[dryso2$region=="NH_pacific"] <- dryso2$value[dryso2$region=="NH_pacific"]*pacific_area*31536000
+dryso2$value[dryso2$region=="NH_indian"] <- dryso2$value[dryso2$region=="NH_indian"]*indian_area*31536000
+dryso2$value[dryso2$region=="arctic"] <- dryso2$value[dryso2$region=="arctic"]*arctic_area*31536000
+
+dryso4$value[dryso4$region=="global"] <- dryso4$value[dryso4$region=="global"]*global_area*31536000
+dryso4$value[dryso4$region=="sea"] <- dryso4$value[dryso4$region=="sea"]*sea_area*31536000
+dryso4$value[dryso4$region=="land"] <- dryso4$value[dryso4$region=="land"]*land_area*31536000
+dryso4$value[dryso4$region=="NH_atlantic"] <- dryso4$value[dryso4$region=="NH_atlantic"]*atlantic_area*31536000
+dryso4$value[dryso4$region=="NH_pacific"] <- dryso4$value[dryso4$region=="NH_pacific"]*pacific_area*31536000
+dryso4$value[dryso4$region=="NH_indian"] <- dryso4$value[dryso4$region=="NH_indian"]*indian_area*31536000
+dryso4$value[dryso4$region=="arctic"] <- dryso4$value[dryso4$region=="arctic"]*arctic_area*31536000
+
+wetso2$value[wetso2$region=="global"] <- wetso2$value[wetso2$region=="global"]*global_area*31536000
+wetso2$value[wetso2$region=="sea"] <- wetso2$value[wetso2$region=="sea"]*sea_area*31536000
+wetso2$value[wetso2$region=="land"] <- wetso2$value[wetso2$region=="land"]*land_area*31536000
+wetso2$value[wetso2$region=="NH_atlantic"] <- wetso2$value[wetso2$region=="NH_atlantic"]*atlantic_area*31536000
+wetso2$value[wetso2$region=="NH_pacific"] <- wetso2$value[wetso2$region=="NH_pacific"]*pacific_area*31536000
+wetso2$value[wetso2$region=="NH_indian"] <- wetso2$value[wetso2$region=="NH_indian"]*indian_area*31536000
+wetso2$value[wetso2$region=="arctic"] <- wetso2$value[wetso2$region=="arctic"]*arctic_area*31536000
+
+wetso4$value[wetso4$region=="global"] <- wetso4$value[wetso4$region=="global"]*global_area*31536000
+wetso4$value[wetso4$region=="sea"] <- wetso4$value[wetso4$region=="sea"]*sea_area*31536000
+wetso4$value[wetso4$region=="land"] <- wetso4$value[wetso4$region=="land"]*land_area*31536000
+wetso4$value[wetso4$region=="NH_atlantic"] <- wetso4$value[wetso4$region=="NH_atlantic"]*atlantic_area*31536000
+wetso4$value[wetso4$region=="NH_pacific"] <- wetso4$value[wetso4$region=="NH_pacific"]*pacific_area*31536000
+wetso4$value[wetso4$region=="NH_indian"] <- wetso4$value[wetso4$region=="NH_indian"]*indian_area*31536000
+wetso4$value[wetso4$region=="arctic"] <- wetso4$value[wetso4$region=="arctic"]*arctic_area*31536000
+
+# Multiply standard deviation by constants
+dryso2$sd[dryso2$region=="global"] <- dryso2$sd[dryso2$region=="global"]*global_area*31536000
+dryso2$sd[dryso2$region=="sea"] <- dryso2$sd[dryso2$region=="sea"]*sea_area*31536000
+dryso2$sd[dryso2$region=="land"] <- dryso2$sd[dryso2$region=="land"]*land_area*31536000
+dryso2$sd[dryso2$region=="NH_atlantic"] <- dryso2$sd[dryso2$region=="NH_atlantic"]*atlantic_area*31536000
+dryso2$sd[dryso2$region=="NH_pacific"] <- dryso2$sd[dryso2$region=="NH_pacific"]*pacific_area*31536000
+dryso2$sd[dryso2$region=="NH_indian"] <- dryso2$sd[dryso2$region=="NH_indian"]*indian_area*31536000
+dryso2$sd[dryso2$region=="arctic"] <- dryso2$sd[dryso2$region=="arctic"]*arctic_area*31536000
+
+dryso4$sd[dryso4$region=="global"] <- dryso4$sd[dryso4$region=="global"]*global_area*31536000
+dryso4$sd[dryso4$region=="sea"] <- dryso4$sd[dryso4$region=="sea"]*sea_area*31536000
+dryso4$sd[dryso4$region=="land"] <- dryso4$sd[dryso4$region=="land"]*land_area*31536000
+dryso4$sd[dryso4$region=="NH_atlantic"] <- dryso4$sd[dryso4$region=="NH_atlantic"]*atlantic_area*31536000
+dryso4$sd[dryso4$region=="NH_pacific"] <- dryso4$sd[dryso4$region=="NH_pacific"]*pacific_area*31536000
+dryso4$sd[dryso4$region=="NH_indian"] <- dryso4$sd[dryso4$region=="NH_indian"]*indian_area*31536000
+dryso4$sd[dryso4$region=="arctic"] <- dryso4$sd[dryso4$region=="arctic"]*arctic_area*31536000
+
+wetso2$sd[wetso2$region=="global"] <- wetso2$sd[wetso2$region=="global"]*global_area*31536000
+wetso2$sd[wetso2$region=="sea"] <- wetso2$sd[wetso2$region=="sea"]*sea_area*31536000
+wetso2$sd[wetso2$region=="land"] <- wetso2$sd[wetso2$region=="land"]*land_area*31536000
+wetso2$sd[wetso2$region=="NH_atlantic"] <- wetso2$sd[wetso2$region=="NH_atlantic"]*atlantic_area*31536000
+wetso2$sd[wetso2$region=="NH_pacific"] <- wetso2$sd[wetso2$region=="NH_pacific"]*pacific_area*31536000
+wetso2$sd[wetso2$region=="NH_indian"] <- wetso2$sd[wetso2$region=="NH_indian"]*indian_area*31536000
+wetso2$sd[wetso2$region=="arctic"] <- wetso2$sd[wetso2$region=="arctic"]*arctic_area*31536000
+
+wetso4$sd[wetso4$region=="global"] <- wetso4$sd[wetso4$region=="global"]*global_area*31536000
+wetso4$sd[wetso4$region=="sea"] <- wetso4$sd[wetso4$region=="sea"]*sea_area*31536000
+wetso4$sd[wetso4$region=="land"] <- wetso4$sd[wetso4$region=="land"]*land_area*31536000
+wetso4$sd[wetso4$region=="NH_atlantic"] <- wetso4$sd[wetso4$region=="NH_atlantic"]*atlantic_area*31536000
+wetso4$sd[wetso4$region=="NH_pacific"] <- wetso4$sd[wetso4$region=="NH_pacific"]*pacific_area*31536000
+wetso4$sd[wetso4$region=="NH_indian"] <- wetso4$sd[wetso4$region=="NH_indian"]*indian_area*31536000
+wetso4$sd[wetso4$region=="arctic"] <- wetso4$sd[wetso4$region=="arctic"]*arctic_area*31536000
+
+## Plot
+wetso2 <- wetso2 %>% filter(model!="E3SM")
+wetso2_basin_plot  <- plot_species(wetso2, region, value, 'SO2 Wet Deposition', expression(Delta*~wetso2~(kg)), exper, model_colors, model_symbols)
+dryso2_basin_plot  <- plot_species(dryso2, region, value, 'SO2 Dry Deposition', expression(Delta*~dryso2~(kg)), exper, model_colors, model_symbols)
+wetso4_basin_plot  <- plot_species(wetso4, region, value, 'SO4 Wet Deposition', expression(Delta*~wetso4~(kg)), exper, model_colors, model_symbols)
+dryso4_basin_plot  <- plot_species(dryso4, region, value, 'SO4 Dry Deposition', expression(Delta*~dryso4~(kg)), exper, model_colors, model_symbols)
+
+# Save so2/so4 plots together
+deposition_basin_plots <- grid_arrange_shared_legend(dryso2_basin_plot,wetso2_basin_plot,
+                                                     dryso4_basin_plot,wetso4_basin_plot)
+png(paste0(emi_dir,"/output/deposition_basin_plots_shp-60p-red.png"))
+grid.draw(deposition_basin_plots)
+dev.off()
+
+so2_deposition <- left_join(wetso2,dryso2,by=c("model","region"))
+so2_deposition$total_deposition <- so2_deposition$value.x + so2_deposition$value.y
+so4_deposition <- left_join(wetso4,dryso4,by=c("model","region"))
+so4_deposition$total_deposition <- so4_deposition$value.x + so4_deposition$value.y
